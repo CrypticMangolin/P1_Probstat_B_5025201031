f03c8b51c4f00db86e593540d423c497c58e4f8b
Hidayatullah <80145586+CrypticMangolin@users.noreply.github.com>
2022-04-10 22:02:04 +0700

    Update Soal5.R

--- Soal5.R
+++ Soal5.R
@@ -1,13 +1,21 @@
 # No5
-lambda = 3
+b = 3
 
 # 5a.
-dexp(lambda)
+dexp(b)
 
 # 5b.
 set.seed(1)
-hist(rexp(10, x), main = 'Distribusi Eksponensial 10 Data')
-hist(rexp(100, x), main = 'Distribusi Eksponensial 100 Data')
-hist(rexp(1000, x), main = 'Distribusi Eksponensial 1000 Data')
-hist(rexp(10000, x), main = 'Distribusi Eksponensial 10000 Data')
+hist(rexp(10, b), main = 'Distribusi Eksponensial 10 Data')
+hist(rexp(100, b), main = 'Distribusi Eksponensial 100 Data')
+hist(rexp(1000, b), main = 'Distribusi Eksponensial 1000 Data')
+hist(rexp(10000, b), main = 'Distribusi Eksponensial 10000 Data')
 
+# 5c 
+# Mean 
+mu = b
+mu
+
+# Variance
+sigma.sq = b^2
+sigma.sq 
